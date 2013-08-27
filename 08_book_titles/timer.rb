75188d12cf7eb983f983fa54fab985c0b78a6048
ahmedhawas <ahmedhawas7@gmail.com>
2013-08-26 22:29:04 -0400

    book_titles solution commit

--- 08_book_titles/timer.rb
+++ 08_book_titles/timer.rb
@@ -2,13 +2,31 @@ class Timer
 	def initialize
 		@seconds=0
 	end
+	
+	attr_accessor :seconds 
 
-	def seconds
-		return @seconds
+	def time_string
+		hours=@seconds /3600
+		minutes= (@seconds - hours*3600)/60
+		sec = (@seconds -hours*3600) - minutes*60
+
+		return "#{padded(hours)}:#{padded(minutes)}:#{padded(sec)}"
+
 	end
 
-	def seconds= (timer)
-		@seconds = timer
+	def padded(n)
+		if (n < 10)
+			return "0"+n.to_s
+		else
+			return n.to_s
+		end
 	end
+	
+	# def seconds
+	# 	return @seconds
+	# end
 
+	# def seconds= (timer)
+	# 	@seconds = timer
+	# end
 end
